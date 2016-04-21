3387e1771232429f323f599aece1b92e5d348466
George Madrid <gmadrid@gmail.com>
2016-04-21 10:51:00 -0400

    Initial Commit

---
+++ jswiftson/jswiftson.h
@@ -0,0 +1,19 @@
+//
+//  jswiftson.h
+//  jswiftson
+//
+//  Created by George Madrid on 4/21/16.
+//  Copyright © 2016 George Madrid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for jswiftson.
+FOUNDATION_EXPORT double jswiftsonVersionNumber;
+
+//! Project version string for jswiftson.
+FOUNDATION_EXPORT const unsigned char jswiftsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jswiftson/PublicHeader.h>
+
+
